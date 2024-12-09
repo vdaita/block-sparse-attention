c6f3ef65302f2a55143db592728982e6113e696c
V Daita <vdaita@gmail.com>
2024-12-08 18:52:13 -0600

    save: evaluation methods for different block sparsity methods

--- bsa_pooling_methods.py
+++ bsa_pooling_methods.py
@@ -1,3 +1,6 @@
+import torch
+from torch.nn.functional import F
+
 # Reshaped appearance: (bsz, length, num_heads, head_dim)
 def get_top_blocks_regular(query_states, key_states, block_size):
     B, H, L, D = query_states.shape
@@ -9,9 +12,13 @@ def get_top_blocks_regular(query_states, key_states, block_size):
     attention = attention.sum(dim=-1)
     return attention
 
-def compare(top_blocks_regular, top_blocks_generated, block_size):
-    g_reshaped = top_blocks_generated...
-
+def compare_divergence(top_blocks_regular, top_blocks_generated, block_size):
+    B, Tadj, D = top_blocks_generated.shape
+    B, T, D = top_blocks_regular.shape
+    g_reshaped = top_blocks_generated.unsqueeze(2).repeat(1, 1, block_size, 1).view(B, Tadj * block_size, D)
+    # calculate the KL divergence between the two distributions
+    kl_div = F.kl_div(F.log(top_blocks_regular + 1e-8), g_reshaped, reduction='mean')
+    return kl_div.item()
 
 def avg_softmax_pooling(query_states, key_states, block_size):
     B, H, L, D = query_states.shape
@@ -21,9 +28,13 @@ def avg_softmax_pooling(query_states, key_states, block_size):
     key_reshaped = key_states.reshape(B * H, L // block_size, block_size, D)
     query_reshaped = query_reshaped.mean(dim=-2)
     key_reshaped = key_reshaped.mean(dim=-2)
-    attention = torch.einsum("bld,bld->bll", query_reshaped, key_reshaped)
-    attention = attention.softmax(dim=-1)
-    return attention
+    attn_weights = torch.einsum("bld,bld->bll", query_reshaped, key_reshaped)
+    # Create and add causal mask
+    mask = torch.triu(torch.ones(L // block_size, L // block_size), diagonal=1).to(attn_weights.device)
+    mask = mask.unsqueeze(0).unsqueeze(0)
+    attn_weights = attn_weights.masked_fill(mask == 1, float('-inf'))
+    attn_weights = attn_weights.softmax(dim=-1)
+    return attn_weights
 
 def softmax_avg_pooling(query_states, key_states, top_k, block_size):
     ...
@@ -39,3 +50,7 @@ def avg_pooling(query_states, key_states, top_k, block_size):
 
 def max_pooling(query_states, key_states, top_k, block_size):
     ...
+
+pooling_methods = {
+    "avg_softmax_pooling": avg_softmax_pooling,
+}
--- evaluate_bsa.py
+++ evaluate_bsa.py
@@ -4,12 +4,105 @@ import torch
 from time import perf_counter
 import json
 from tqdm import tqdm
+from dataclasses import dataclass
+from torch import nn
+from typing import Optional, Tuple
+from transformers.models.llama.modeling_llama import LlamaForCausalLM, LlamaConfig, LlamaRotaryEmbedding, apply_rotary_pos_emb, repeat_kv
+from transformers.cache_utils import Cache
+import math
+from bsa_pooling_methods import get_top_blocks_regular, compare_divergence, pooling_methods
+from typing import List
 
-model_name = "gpt2"
+@dataclass
+class Result():
+    method_name: str
+    divergence: float
+    block_size: int
+    layer_index: int
+
+model_name = "meta-llama/Llama-3.2-1B"
+dataset_name = ""
+block_size = 64
+
+results: List[Result] = []
+
+def custom_forward(
+    self,
+    hidden_states: torch.Tensor,
+    attention_mask: Optional[torch.Tensor] = None,
+    position_ids: Optional[torch.LongTensor] = None,
+    past_key_value: Optional[Cache] = None,
+    output_attentions: bool = False,
+    use_cache: bool = False,
+    cache_position: Optional[torch.LongTensor] = None,
+    position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,  # will become mandatory in v4.46
+    **kwargs,
+) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+    bsz, q_len, _ = hidden_states.size()
+
+    query_states = self.q_proj(hidden_states)
+    key_states = self.k_proj(hidden_states)
+    value_states = self.v_proj(hidden_states)
+
+    # use -1 to infer num_heads and num_key_value_heads as they may vary if tensor parallel is used
+    query_states = query_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+    key_states = key_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+    value_states = value_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+
+    if position_embeddings is None:
+        cos, sin = self.rotary_emb(value_states, position_ids)
+    else:
+        cos, sin = position_embeddings
+    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+
+    if past_key_value is not None:
+        # sin and cos are specific to RoPE models; cache_position needed for the static cache
+        cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
+        key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+    key_states = repeat_kv(key_states, self.num_key_value_groups)
+    value_states = repeat_kv(value_states, self.num_key_value_groups)
+    attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+
+    if attention_mask is not None:  # no matter the length, we just slice it
+        causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+        attn_weights = attn_weights + causal_mask
+
+    # upcast attention to fp32
+    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+    
+    # use this
+    regular_blocks = get_top_blocks_regular(query_states, key_states, block_size)
+    for method in pooling_methods:
+        method_blocks = pooling_methods[method](query_states, key_states, block_size)
+        divergence = compare_divergence(regular_blocks, method_blocks, block_size)
+        results.append(Result(method, divergence, block_size, self.layer_idx))
+    
+    attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+    attn_output = torch.matmul(attn_weights, value_states)
+
+    if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+        raise ValueError(
+            f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+            f" {attn_output.size()}"
+        )
+
+    attn_output = attn_output.transpose(1, 2).contiguous()
+
+    attn_output = attn_output.reshape(bsz, q_len, -1)
+
+    attn_output = self.o_proj(attn_output)
+
+    if not output_attentions:
+        attn_weights = None
+
+    return attn_output, attn_weights, past_key_value
+
+LlamaForCausalLM.forward = custom_forward
 
 model = AutoModelForCausalLM.from_pretrained(model_name) 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-ds = load_dataset("")
+ds = load_dataset(dataset_name)
 
 prompts = []
 for split in ds:
@@ -18,51 +111,9 @@ for split in ds:
     prompts.append(prompt)
 
 num_prompts = len(prompts)
-mean_squared_error = [{}]
-
-baseline_attention_results = []
-baseline_attention_time = 0.0
-
-sparsities = [0.75, 0.5, 0.25, 0.2, 0.15, 0.1, 0.05]
-
-attention_approaches = [
-
-] # block size can be 16 (custom kernel), or 64 (flex-attention)
-
-mse = {
-    sparsity: {
-        approach: [] for approach in list(attention_approaches.keys())
-    } for sparsity in sparsities
-}
-
-for start in range(0, num_prompts, 4):
-    end = min(start + 10, num_prompts)
-    batch = prompts[start:end]
+for prompt in tqdm(prompts):
+    inputs = tokenizer(prompt, return_tensors="pt", max_length=16384, padding="max_length")
+    outputs = model(**inputs)
     
-    # Perform a single forward pass through the model
-    batch_tokenized = tokenizer(batch, return_tensors="pt", padding=True)
-    print("Tokenized matrix: ", batch_tokenized)
-    start_time = perf_counter()
-    state = model.model.embed_tokens(batch_tokenized)
-    with torch.no_grad():
-        for layer_idx, layer in enumerate(model.model.layers):
-            state = layer(state)
-            print(f"State at layer {layer_idx} ", state)
-            baseline_attention_results.append(state)
-    baseline_attention_time = perf_counter() - start_time
-    
-    # then, evaluate the attention mechanisms using various monkeypatches
-    for approach in tqdm(attention_approaches):
-        # TODO: apply the monkeypatch to the model
-        start_time = perf_counter()
-        state = model.model.embed_tokens(batch_tokenized)
-        with torch.no_grad():
-            for layer_idx, layer in enumerate(model.model.layers):
-                state = layer(state)
-                print(f"State at layer {layer_idx} ", state)
-                # Calculate the mean squared error between the baseline and the monkeypatched attention mechanisms
-                baseline_attention_results.append(state)
-        end_time = perf_counter() - start_time
-
-with open("bsa_results.json", "w") as f:
-    json.dump(mse, f)
+with open(f"bsa_results_{block_size}.json", "w+") as f:
+    f.write(json.dumps([result.__dict__ for result in results], indent=2))
